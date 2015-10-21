8d165cfd3b6a1e1ee5e45fd3308f6c2339ca8de5
Daniel Dickison <danieldickison@gmail.com>
2015-10-20 23:41:26 -0400

    Automatic conversion for Swift 2

--- ObserverSet/ObserverSet.swift
+++ ObserverSet/ObserverSet.swift
@@ -22,7 +22,7 @@ public class ObserverSetEntry<Parameters> {
 }
 
 
-public class ObserverSet<Parameters>: Printable {
+public class ObserverSet<Parameters>: CustomStringConvertible {
     // Locking support
     
     private var queue = dispatch_queue_create("com.mikeash.ObserverSet", nil)
@@ -106,7 +106,7 @@ public class ObserverSet<Parameters>: Printable {
                 ? "\(entry.f)"
                 : "\(entry.object) \(entry.f)")
         }
-        let joined = ", ".join(strings)
+        let joined = strings.joinWithSeparator(", ")
         
         return "\(reflect(self).summary): (\(joined))"
     }
